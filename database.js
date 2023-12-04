ff49fe4b3eafc49511665d10f1d1833312e549dc
touunix <touunix@gmail.com>
2023-12-04 18:36:51 +0100

    Add database connector

---
+++ database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+class DatabaseConnector {
+    constructor(database_name, database_user, database_password){
+        this.database_name = database_name;
+        this.database_user = database_user;
+        this.database_password = database_password;
+        this.connection_adress = `mongodb+srv://${this.database_user}:${this.database_password}@cluster0.4ispprf.mongodb.net/${this.database_name}?retryWrites=true&w=majority`
+    }
+
+    connect() {
+        mongoose.connect(this.connection_adress)
+        .then(() => console.log("SUCCESSFULLY connected to the Database!"));
+    };
+}
+
+module.exports = DatabaseConnector;
